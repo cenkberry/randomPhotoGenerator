d7ab9bfe2ed8ad6918a646ccfb5480022495242a
Cenk Beri <cenkberi@gmail.com>
2022-01-22 19:28:29 +0300

    designed

--- js/main.js
+++ js/main.js
@@ -2,9 +2,8 @@ const screen = document.querySelector('#screen');
 const button = document.querySelector('#generate');
 
 button.addEventListener("click", () => {
-
+    
     const quant = document.querySelector('#quant').value;
-
     screen.innerHTML = "";
 
     for (let i = 0; i <= quant; i++) {
@@ -12,11 +11,11 @@ button.addEventListener("click", () => {
         let randomNUM1 = Math.floor(Math.random() * 10 + 1);
         let randomNUM2 = Math.floor(Math.random() * 10 + 1);
         let randomNUM3 = Math.floor(Math.random() * 1000 + 100);
-        
+
         let imgid   = "id" + randomNUM1;
         let durtime = quant/3+randomNUM1 + "s";
         let deltime = quant/15+randomNUM2 + "s";
-        
+
         let image =  document.createElement('img');
         image.src   =  `https://picsum.photos/${randomNUM3}/400/`;
         image.className = "block img-thumbnail rounded " + imgid  ;
@@ -27,6 +26,5 @@ button.addEventListener("click", () => {
         image.style.animationDelay = deltime;
         screen.append(image);
         console.log(randomNUM1 , randomNUM2 , randomNUM3 , imgid)
-
     }
 });
